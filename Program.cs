005136dc20f87ce01b3f00837df696ab38bfd139
Logan King <logan.ramzie.king@gmail.com>
2024-02-18 23:25:38 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Author: Logan King
+ * Course: COMP-03A
+ * Purpouse: To show our ability with Methods and XML comments
+ */
+
+namespace COMP003A.Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+        }
+        static void PrintSeparator()
+        {
+            Console.WriteLine("".PadRight(50, '*'));
+        }
+
+        static void PrintSeparator(string fake)
+        {
+            
+
+        }
+    }
+}
