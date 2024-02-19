1190dceb3812d5a85bc8c9f1032c5eac5d714e5b
Logan King <logan.ramzie.king@gmail.com>
2024-02-18 23:59:31 -0800

    unable to make int size work

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
  * Purpouse: To show our ability with Methods and XML comments
  */
 
+using System.Transactions;
+
 namespace COMP003A.Assignment5
 {
     internal class Program
@@ -19,8 +21,31 @@ namespace COMP003A.Assignment5
 
         static void PrintSeparator(string fake)
         {
-            
+            PrintSeparator();
+            Console.WriteLine(fake);
+            PrintSeparator();   
+            /// <summary>
+            /// A print seportator that dosnt go anywhwere
+            /// </summary>
+        }
 
+
+        static void IsoscelesTriangleBuilder(char inputCharecter, int size)
+        {
+            Console.WriteLine("\n Please input ONE charecter");
+            string inputCharercter = Console.ReadLine();
+
+            Console.WriteLine("\n Please enter a positive whole number");
+            
+            int size = Console.ReadLine();
+            
+          
+            
+            
+            for (int i = 0; i < size; i++) 
+            { 
+                
+            }
         }
     }
 }
