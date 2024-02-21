c3ee98577381d500b2cd17fae81c967267a1b641
Logan King <logan.ramzie.king@gmail.com>
2024-02-21 14:07:28 -0800

    Everything made just the few errors i cant find a way around

--- Program.cs
+++ Program.cs
@@ -9,55 +9,91 @@ namespace COMP003A.Assignment5
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        void Main(string[] args)
         {
-            
+            PrintSeparator("Triangle");
+            {
+                Console.WriteLine("\t Please input ONE of any charecter");
+                var inputLetter = Console.ReadLine();
+
+                Console.WriteLine("\t Please input a POSITIVE WHOLE NUMBER");
+                var inputNumber = Console.ReadLine();
+
+                IsoscelesTriangleBuilder(inputLetter, inputNumber);
+
+            }
+
+            PrintSeparator("Favorite Charecter");
+            {
+
+                CharacterInfo(Tourge, 1981);
+
+                CharacterInfo(Tina, 2004);
+
+                CharacterInfo(ClapTrap, 2017);
+
+                CharacterInfo(Brick, 2022);
+
+                CharacterInfo(Maya, 1994);
+            }
         }
-        static void PrintSeparator()
+        void PrintSeparator()
         {
             Console.WriteLine("".PadRight(50, '*'));
         }
 
-        static void PrintSeparator(string fake)
+        void PrintSeparator(string fake)
         {
             PrintSeparator();
             Console.WriteLine(fake);
-            PrintSeparator();   
+            PrintSeparator();
             /// <summary>
             /// A print seportator that dosnt go anywhwere
             /// </summary>
         }
 
 
-        static void IsoscelesTriangleBuilder(char inputCharecter, int size)
+        void IsoscelesTriangleBuilder(char inputCharecter, int size)
         {
-            Console.WriteLine("******************************************************");
-            Console.WriteLine("Triangle Section");
-            Console.WriteLine("******************************************************");
-
-
-            Console.WriteLine("\n Please input ONE charecter");
-            
-
-            Console.WriteLine("\n Please enter a positive whole number");
-           
-           
-            for (int i = 1; i<=size ; i++)
+            for (int i = 1; i <= size; i++)
             {
-                for (int j = 1; j<=size; j++)
+                for (int j = 1; j <= size; j++)
                 {
                     Console.WriteLine($"{inputCharecter}");
                 }
             }
 
-            for (int i = 1;i<=size; i++)
+            for (int i = 1; i <= size; i++)
             {
-                for (int j = 1; j<=size-1; j++)
+                for (int j = 1; j <= size - 1; j++)
                 {
                     Console.WriteLine($"{inputCharecter}");
                 }
             }
 
+            /// <summary>
+            /// A triangle genorator taking a custom charecter and number from a user.
+            /// </summary>
         }
+
+        void AgeCaculator(int year)
+        { 
+            int currentYear = 2024 - Convert.ToInt32(year);
+
+            ///<summary>
+            ///A Age calcualor
+            /// </summary>
+        }
+
+        void CharacterInfo(string name, int birthYear)
+        {
+            AgeCaculator(birthYear);
+            Console.WriteLine($"{name} is {birthYear}!");
+            ///<summary>
+            ///A input for the name and print of how old they are
+            /// </summary>
+        }
+
+
     }
 }
