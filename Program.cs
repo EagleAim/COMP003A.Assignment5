f71b2fdf3dd594379c1a5637ac2bbf3a14a4d7b3
Logan King <logan.ramzie.king@gmail.com>
2024-02-22 16:20:14 -0800

    Ages fixed

--- Program.cs
+++ Program.cs
@@ -9,40 +9,44 @@ namespace COMP003A.Assignment5
 {
     internal class Program
     {
-        void Main(string[] args)
+        static void Main(string[] args)
         {
             PrintSeparator("Triangle");
             {
                 Console.WriteLine("\t Please input ONE of any charecter");
                 var inputLetter = Console.ReadLine();
+                char letter = Convert.ToChar(inputLetter);
+
 
                 Console.WriteLine("\t Please input a POSITIVE WHOLE NUMBER");
                 var inputNumber = Console.ReadLine();
+                int number = Convert.ToInt32(inputNumber);
+               
 
-                IsoscelesTriangleBuilder(inputLetter, inputNumber);
+                IsoscelesTriangleBuilder(letter, number);
 
             }
 
             PrintSeparator("Favorite Charecter");
             {
 
-                CharacterInfo(Tourge, 1981);
+                CharacterInfo("Tourge", 1981);
 
-                CharacterInfo(Tina, 2004);
+                CharacterInfo("Tina", 2004);
 
-                CharacterInfo(ClapTrap, 2017);
+                CharacterInfo("ClapTrap", 2017);
 
-                CharacterInfo(Brick, 2022);
+                CharacterInfo("Brick", 2002);
 
-                CharacterInfo(Maya, 1994);
+                CharacterInfo("Maya", 1994);
             }
         }
-        void PrintSeparator()
+        static void PrintSeparator()
         {
             Console.WriteLine("".PadRight(50, '*'));
         }
 
-        void PrintSeparator(string fake)
+        static void PrintSeparator(string fake)
         {
             PrintSeparator();
             Console.WriteLine(fake);
@@ -53,7 +57,7 @@ namespace COMP003A.Assignment5
         }
 
 
-        void IsoscelesTriangleBuilder(char inputCharecter, int size)
+        static void IsoscelesTriangleBuilder(char inputCharecter, int size)
         {
             for (int i = 1; i <= size; i++)
             {
@@ -76,7 +80,7 @@ namespace COMP003A.Assignment5
             /// </summary>
         }
 
-        void AgeCaculator(int year)
+        static void AgeCaculator(int year)
         { 
             int currentYear = 2024 - Convert.ToInt32(year);
 
@@ -85,10 +89,11 @@ namespace COMP003A.Assignment5
             /// </summary>
         }
 
-        void CharacterInfo(string name, int birthYear)
+        static void CharacterInfo(string name, int birthYear)
         {
             AgeCaculator(birthYear);
-            Console.WriteLine($"{name} is {birthYear}!");
+            int yearBorn = 2024 - Convert.ToInt32(birthYear);
+            Console.WriteLine($"{name} is {yearBorn}!");
             ///<summary>
             ///A input for the name and print of how old they are
             /// </summary>
